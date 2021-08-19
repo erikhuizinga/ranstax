b96ceaf83cbb0173f8c06fee262d18f9b6a28668
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2021-08-19 09:00:51 +0200

    Implement taking from random stack in session

--- src/commonMain/kotlin/com/github/erikhuizinga/ranstax/Session.kt
+++ src/commonMain/kotlin/com/github/erikhuizinga/ranstax/Session.kt
@@ -1,14 +1,29 @@
 package com.github.erikhuizinga.ranstax
 
 /**
- * @constructor Throws an [IllegalArgumentException] if [stacks] is empty.
+ * @throws IllegalArgumentException If [stacks] is empty.
  * @param stacks Must not be empty.
  * @property stacks The [Stacks][Stack] in this session.
  */
-class Session(val stacks: List<Stack>) {
+class Session(var stacks: Collection<Stack>) {
     constructor(vararg stacks: Stack) : this(stacks.toList())
 
     init {
-        require(stacks.isNotEmpty())
+        require(stacks.isNotEmpty()) { "stacks must not be empty" }
+    }
+
+    private var stack = NodeStack(stacks)
+
+    /**
+     * Take an item from a random stack.
+     * This function updates [stacks].
+     *
+     * @return The stack from which the item was taken.
+     */
+    fun takeFromRandomStack(): Stack {
+        val preStacks = stacks
+        stack = nodeStackReducer(stack)
+        stacks = stack.stacks
+        return stacks.minus(preStacks).single()
     }
 }
--- src/commonTest/kotlin/com/github/erikhuizinga/ranstax/SessionTest.kt
+++ src/commonTest/kotlin/com/github/erikhuizinga/ranstax/SessionTest.kt
@@ -1,12 +1,59 @@
 package com.github.erikhuizinga.ranstax
 
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 internal class SessionTest {
     @Test
     fun sessionConstructorWithoutStacksThrowsIAE() {
-        assertFailsWith<IllegalArgumentException> { Session() }
-        assertFailsWith<IllegalArgumentException> { Session(emptyList()) }
+        val expected = "stacks must not be empty"
+        val illegalArgumentException1 = assertFailsWith<IllegalArgumentException> { Session() }
+        assertEquals(expected, actual = illegalArgumentException1.message)
+        val illegalArgumentException2 = assertFailsWith<IllegalArgumentException> {
+            Session(emptyList())
+        }
+        assertEquals(expected, actual = illegalArgumentException2.message)
+    }
+
+    @Test
+    fun sessionWithSingleStack_TakingReturnsSameStackWithDecrementedSize() {
+        val size = 1
+        val stack = LeafStack(size = size)
+        val session = Session(stack)
+        val expected = stack.copy(size = size - 1)
+
+        val actual = session.takeFromRandomStack()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun sessionWithMultipleStacks_RepeatedlyTakingReturnsAllStacksRandomly() {
+        fun List<Stack>.sort() =
+            sortedWith(compareBy(Stack::name).thenByDescending(Stack::size))
+
+        val nStacks = 50
+
+        val stacks = List(nStacks) {
+            val size = it + 1
+            Stack(name = size.toString(), size = size)
+        }
+        val totalSize = stacks.sumOf(Stack::size)
+        val session = Session(stacks)
+        val expected = stacks.flatMap { stack ->
+            sequence {
+                var theStack = stack
+                repeat(theStack.size) {
+                    theStack = defaultStackReducer(theStack)
+                    yield(theStack)
+                }
+            }
+        }.sort()
+
+        val actual = List(totalSize) { session.takeFromRandomStack() }.sort()
+
+        assertContentEquals(expected, actual)
     }
 }
