210c0379fc9ffd0f496e38577ad87a7020154654
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2024-01-11 21:04:33 +0100

    rm

--- src/jsMain/kotlin/com/github/erikhuizinga/ranstax/domain/HasHint.kt
+++
@@ -1,5 +0,0 @@
-package com.github.erikhuizinga.ranstax.domain
-
-interface HasHint {
-    val hint: String?
-}
--- src/jsMain/kotlin/com/github/erikhuizinga/ranstax/domain/Validator.kt
+++
@@ -1,5 +0,0 @@
-package com.github.erikhuizinga.ranstax.domain
-
-interface Validator<in I, out O> {
-    operator fun invoke(obj: I): O
-}
