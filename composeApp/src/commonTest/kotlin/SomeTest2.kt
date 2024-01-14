32e31c1c7172922a43636f692627d57e3a577c49
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2024-01-14 08:33:39 +0100

    remove

--- composeApp/src/commonTest/kotlin/SomeTest2.kt
+++
@@ -1,7 +0,0 @@
-import kotlin.test.Test
-import kotlin.test.assertTrue
-
-class SomeTest2 {
-    @Test
-    fun test() = assertTrue(false)
-}
--- composeApp/src/wasmJsTest/kotlin/SomeTest.kt
+++
@@ -1,7 +0,0 @@
-import kotlin.test.Test
-import kotlin.test.assertTrue
-
-class SomeTest {
-    @Test
-    fun test() = assertTrue(false)
-}
